cc9251fe34c74b21a3d339048e0dde96d7e0819a
한다혜/글로벌개발팀 <139421887+dahyehan22@users.noreply.github.com>
2023-11-27 15:37:36 +0900

    feat : gulp 환경세팅 수정

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,67 @@
-const gulp = require('gulp');
-const inlineCss = require('gulp-inline-css');
-const sass = require('gulp-sass');
-const browserSync = require('browser-sync').create();
-const plumber = require('gulp-plumber');
+const gulp = require("gulp");
+const inlineCss = require("gulp-inline-css");
+const sass = require("gulp-sass");
+const browserSync = require("browser-sync").create();
+const plumber = require("gulp-plumber");
+const fileinclude = require("gulp-file-include");
+const PATH = {
+  HTML: "src/html",
+  SCSS: "src/scss",
+};
+const DEST_PATH = {
+  HTML: "build/html",
+  CSS: "build/css/",
+};
 
-gulp.task('serve', () => {
-	browserSync.init({
-		server: ''
-	})
-	gulp.watch('**/**/*.html' ,['inline']).on('change', browserSync.reload)	
-})
-
-gulp.task('scss', () => {
-	gulp.src('src/scss/**/*.scss')
-	.pipe(plumber())
-	.pipe(sass())
-	.pipe(gulp.dest('build/css/'))
-	.pipe(browserSync.stream())
-})
-
-
-gulp.task('inline', () => {
-	gulp.src('./index.html')
-	.pipe(inlineCss({
-		removeHtmlSelectors: true
-	}))
-	.pipe(gulp.dest('build/'))
+gulp.task("serve", () => {
+  browserSync.init({
+    server: "",
+  });
+  gulp
+    .watch("**/**/*.html", ["html", "inline", "scss"])
+    .on("change", browserSync.reload);
 });
 
-gulp.task('default', ['serve', 'scss'], () => {
-	gulp.watch('./src/scss/**/*.scss', ['scss','inline'])
+gulp.task("scss", () => {
+  gulp
+    .src(PATH.SCSS + "**/*.scss")
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(gulp.dest(DEST_PATH.CSS))
+    .pipe(browserSync.stream());
+});
+
+gulp.task("inline", () => {
+  gulp
+    .src(DEST_PATH.HTML + "/*.html")
+    .pipe(
+      inlineCss({
+        removeHtmlSelectors: true,
+      })
+    )
+    .pipe(gulp.dest("build/"));
+});
+
+gulp.task("html", () => {
+  return new Promise((resolve) => {
+    gulp
+      .src(PATH.HTML + "/*.html")
+      .pipe(
+        fileinclude({
+          prefix: "@@",
+          basepath: "@file",
+        })
+      )
+      .pipe(gulp.dest(DEST_PATH.HTML))
+      .pipe(
+        browserSync.reload({
+          stream: true,
+        })
+      );
+    resolve();
+  });
+});
+
+gulp.task("default", ["serve", "scss"], () => {
+  gulp.watch("./src/scss/**/*.scss", ["scss", "inline", "html"]);
 });
